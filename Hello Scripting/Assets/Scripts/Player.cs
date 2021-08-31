0527108c50ec6342d93097b2ea8d5952938508c0
Mike Allison <philosopher.osopher@gmail.com>
2021-08-31 16:37:16 +0400

    completed input

--- Hello Scripting/Assets/Scripts/Player.cs
+++ Hello Scripting/Assets/Scripts/Player.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
 
+    public float speed = 100.0f;
     Rigidbody rb;
 
     // Start is called before the first frame update
@@ -16,7 +18,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 randDir = Random.onUnitSphere;
-        rb.AddForce(new Vector3(randDir.x, 0, randDir.y) * Random.Range(1,100));
+        //Vector3 randDir = Random.onUnitSphere;
+        //rb.AddForce(new Vector3(randDir.x, 0, randDir.y) * Random.Range(1,100));
+    }
+
+    void OnMove(InputValue input)
+    {
+        Vector2 dir = input.Get<Vector2>();
+        rb.AddForce(new Vector3(dir.x, 0, dir.y) * speed);
     }
 }
